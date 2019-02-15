6e333ce512133cf18f091016736107e3f976bada
ayoubhamaoui <ayoub10hamaoui@gmaiL.com>
2019-02-15 17:47:45 +0100

    first commit

---
+++ GNE/DAO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GNE
+{
+    public class DAO : MySQL
+    {
+        public string[] param_co;
+        private string Table;
+
+        public DAO(string bd, string serv, string user, string pass):base (bd,serv,user,pass)
+        {
+            
+        }
+
+        public List<Dictionary<string, string>> select()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int Update()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ GNE/MySQL.cs
@@ -0,0 +1,38 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GNE
+{
+    public abstract class MySQL
+    {
+        private string BD;
+        private string Serv;
+        private MySqlConnection con;
+        private string Pass;
+        private MySqlDataReader rs;
+        private MySqlCommand st;
+        private string User;
+
+
+        protected MySQL(string bd,string serv,string user,string pass)
+        {
+            this.BD = bd;
+            this.Serv = serv;
+            this.User = user;
+            this.Pass = pass;
+        }
+
+        public List<Dictionary<string, string>> Get()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int Up()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
